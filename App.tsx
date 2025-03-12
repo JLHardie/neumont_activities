48e7cc3a4d73c639f3d56fa94c8190ca7f24bfd0
VIROS <123793529+JLHardie@users.noreply.github.com>
2025-03-12 01:49:44 -0600

    cleared and refreshed. testing something

--- App.tsx
+++ App.tsx
@@ -1,38 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-// Import your screens
-import HomeScreen from './screens/HomeScreen';
-import CoordinateScreen from './screens/CoordinateScreen';
-import EventsScreen from './screens/EventsScreen';/* 
-// import CalendarScreen from './screens/CalendarScreen';
-// import CoordinateScreen from './screens/CoordinateScreen';
-// import SettingsScreen from './screens/SettingsScreen';
-// import AdminScreen from './screens/AdminScreen'; */
-//import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    //<Authenticator.Provider>
-    //  <Authenticator>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Events" component={EventsScreen} />
-            <Stack.Screen name="Coordinate" component={CoordinateScreen} />
-            {/* <Stack.Screen name="Calendar" component={CalendarScreen} />
-            <Stack.Screen name="Settings" component={SettingsScreen} />
-            <Stack.Screen name="Admin" component={AdminScreen} /> */}
-          </Stack.Navigator>
-        </NavigationContainer>
-    //  </Authenticator>
-    //</Authenticator.Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
